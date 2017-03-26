e86b51c7df1ebaeb1700bec788e95d214b00c2d3
p-mazhnik <mazhnik.pa@phystech.edu>
2017-03-26 20:16:04 +0300

    Приступил к Vector

---
+++ Geometry_26.03/Vector/Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by pavel on 26.03.2017.
+//
+
+#include "Vector.h"
---
+++ Geometry_26.03/Vector/Vector.h
@@ -0,0 +1,14 @@
+//
+// Created by pavel on 26.03.2017.
+//
+
+#ifndef MIPT_CPP_VECTOR_H
+#define MIPT_CPP_VECTOR_H
+
+
+class Vector {
+
+};
+
+
+#endif //MIPT_CPP_VECTOR_H
